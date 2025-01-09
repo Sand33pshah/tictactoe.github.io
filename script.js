30c2db2442dfd42534841c022b46a565dbc0bf6c
Sand33pshah <shahsandeep980@gmail.com>
2025-01-09 12:56:49 +0530

    winner checking functionality added

--- script.js
+++ script.js
@@ -24,9 +24,48 @@ function startNow() {
 let currentPlayer = 'X'; 
 function makeMove(cell) { 
     if (cell.innerHTML === '') { 
-        cell.innerHTML = currentPlayer; 
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X'; 
+        cell.innerHTML = currentPlayer;
+        if(checkWinner()){
+            // currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            alert(currentPlayer + ' wins !');
+            // reset();
+        } else{
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            document.getElementById('currPlayer').innerHTML = 'Current Player: ' + currentPlayer;
+        } 
     } 
+}
+
+function checkWinner(){
+    const allCell = document.getElementsByClassName('cell');
+    const winningCombination = [
+        [0,1,2], [3,4,5], [6,7,8], //Rows
+        [0,3,6], [1,4,7], [2,5,8], //Columns
+        [0,4,8], [2,4,6]           //Diagonals
+    ];
+
+    for(const combination of winningCombination){
+        const[a,b,c] = combination;
+        if(allCell[a].innerHTML === 'X' && allCell[b].innerHTML === 'X' && allCell[c].innerHTML === 'X') {
+            allCell[a].style.color = '#03C03C';
+            allCell[b].style.color = '#03C03C';
+            allCell[c].style.color = '#03C03C';
+            allCell[a].style.boxShadow = '2px 2px 12px #03C03C';
+            allCell[b].style.boxShadow = '2px 2px 12px #03C03C';
+            allCell[c].style.boxShadow = '2px 2px 12px #03C03C';
+            return true;
+        }else if(allCell[a].innerHTML === 'O' && allCell[b].innerHTML === 'O' && allCell[c].innerHTML === 'O'){
+            allCell[a].style.color = '#03C03C';
+            allCell[b].style.color = '#03C03C';
+            allCell[c].style.color = '#03C03C';
+            allCell[a].style.boxShadow = '2px 2px 12px #03C03C';
+            allCell[b].style.boxShadow = '2px 2px 12px #03C03C';
+            allCell[c].style.boxShadow = '2px 2px 12px #03C03C';
+            return true;
+        }
+    }
+
+    return false;
 }
 
 function goBack() {
@@ -39,9 +78,11 @@ function reset() {
     const allCell = document.getElementsByClassName('cell');
     for(const cell of allCell) {
         cell.innerHTML = '';
+        cell.style.color = '';
+        cell.style.boxShadow = '';
     }
 
-    // currentPlayer = 'X';
+    currentPlayer = 'X';
     // document.getElementById('currPlayer').innerHTML = 'Current Player: ' + currentPlayer;
     console.log('Game Reset');
 }
