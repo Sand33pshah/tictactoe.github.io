b1b012a5569a06c4fca54c313ca81aef3e8434ed
Sand33pshah <shahsandeep980@gmail.com>
2025-01-09 15:29:33 +0530

    added computer logic to the program

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 let currentPlayer = 'X';
 let playerOneName = 'X';
 let PlayerTwoName = 'O';
+let isComputerOpponent = false;
 
 
-function playerMode(){
+function playerMode() {
     const mode = document.getElementById('multiplayer').value;
     const playerTwo = document.getElementById('playerTwo');
     const playerOne = document.getElementById('playerOne');
 
-    if(mode === 'computer'){
+    if (mode === 'computer') {
         playerTwo.style.display = 'none';
         playerOne.placeholder = 'Enter name';
+        isComputerOpponent = true;
     }
-    else{
+    else {
         playerTwo.style.display = 'block';
         playerOne.placeholder = 'Player 1';
+        isComputerOpponent = false;
     }
-    // console.log(mode);
 }
 
 function startNow() {
@@ -26,41 +28,73 @@ function startNow() {
     playerOneName = playerOneInput ? playerOneInput : 'X';
     PlayerTwoName = playerTwoInput ? playerTwoInput : 'O';
 
+    //added by me
+    if (isComputerOpponent) {
+        PlayerTwoName = 'Computer';
+    }
+
     document.getElementById('currPlayer').innerHTML = 'Current Player: ' + playerOneName;
 
     document.getElementsByClassName('welcome')[0].style.display = 'none';
     document.getElementsByClassName('gameFloor')[0].style.display = 'block';
     reset();
-    // console.log('Game On');
 }
 
-
-// let currentPlayer = 'X'; 
-function makeMove(cell) { 
-    if (cell.innerHTML === '') { 
+function makeMove(cell) {
+    if (cell.innerHTML === '') {
         cell.innerHTML = currentPlayer;
-        if(checkWinner()){
+        if (checkWinner()) {
             setTimeout(() => {
                 alert((currentPlayer === 'X' ? playerOneName : PlayerTwoName) + ' wins !');
             }, 200);
-        } else{
+        } else {
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
             document.getElementById('currPlayer').innerHTML = 'Current Player: ' + (currentPlayer === 'X' ? playerOneName : PlayerTwoName);
-        } 
-    } 
+            if (isComputerOpponent && currentPlayer === 'O') {
+                setTimeout(computerMove, 500);
+            }
+        }
+    }
 }
 
-function checkWinner(){
+function computerMove() {
+    const allCells = document.getElementsByClassName('cell');
+    const emptyCells = [];
+    for (let i = 0; i < allCells.length; i++) {
+        if (allCells[i].innerHTML === '') {
+            emptyCells.push(allCells[i]);
+        }
+    }
+    // console.log(Math.floor(Math.random() * emptyCells.length));
+    if (emptyCells.length > 0) {
+        const randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+        randomCell.innerHTML = 'O';
+        if (checkWinner()) {
+            setTimeout(() => {
+                alert(PlayerTwoName + ' wins !');
+            }, 200);
+        } else {
+            currentPlayer = 'X';
+            document.getElementById('currPlayer').innerHTML = 'Current Player: ' + playerOneName;
+        }
+    }
+    console.log(randomCell);
+}
+
+
+
+
+function checkWinner() {
     const allCell = document.getElementsByClassName('cell');
     const winningCombination = [
-        [0,1,2], [3,4,5], [6,7,8], //Rows
-        [0,3,6], [1,4,7], [2,5,8], //Columns
-        [0,4,8], [2,4,6]           //Diagonals
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], //Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], //Columns
+        [0, 4, 8], [2, 4, 6]           //Diagonals
     ];
 
-    for(const combination of winningCombination){
-        const[a,b,c] = combination;
-        if(allCell[a].innerHTML === 'X' && allCell[b].innerHTML === 'X' && allCell[c].innerHTML === 'X') {
+    for (const combination of winningCombination) {
+        const [a, b, c] = combination;
+        if (allCell[a].innerHTML === 'X' && allCell[b].innerHTML === 'X' && allCell[c].innerHTML === 'X') {
             allCell[a].style.color = '#03C03C';
             allCell[b].style.color = '#03C03C';
             allCell[c].style.color = '#03C03C';
@@ -68,7 +102,7 @@ function checkWinner(){
             allCell[b].style.boxShadow = '2px 2px 12px #03C03C';
             allCell[c].style.boxShadow = '2px 2px 12px #03C03C';
             return true;
-        }else if(allCell[a].innerHTML === 'O' && allCell[b].innerHTML === 'O' && allCell[c].innerHTML === 'O'){
+        } else if (allCell[a].innerHTML === 'O' && allCell[b].innerHTML === 'O' && allCell[c].innerHTML === 'O') {
             allCell[a].style.color = '#03C03C';
             allCell[b].style.color = '#03C03C';
             allCell[c].style.color = '#03C03C';
@@ -82,15 +116,17 @@ function checkWinner(){
     return false;
 }
 
+
+
+
 function goBack() {
     document.getElementsByClassName('welcome')[0].style.display = 'flex';
     document.getElementsByClassName('gameFloor')[0].style.display = 'none';
-    console.log('Game On');
 }
 
 function reset() {
     const allCell = document.getElementsByClassName('cell');
-    for(const cell of allCell) {
+    for (const cell of allCell) {
         cell.innerHTML = '';
         cell.style.color = '';
         cell.style.boxShadow = '';
@@ -98,7 +134,6 @@ function reset() {
 
     currentPlayer = 'X';
     document.getElementById('currPlayer').innerHTML = 'Current Player: ' + (currentPlayer === 'X' ? playerOneName : PlayerTwoName);
-    console.log('Game Reset');
 }
 
 
